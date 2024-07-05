480f127a3251bf679c5e8accb16bc34a237ef9de
Tomilola-ng <tee.o2809@gmail.com>
2024-07-05 01:17:41 +0100

    SIMPLIFIED AND TYPED ANNOTATE

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,14 +3,11 @@
     SUM OF ARRAY OF FLOATS WITH TYPE ANNOTATIONS
 """
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
     """
         SUM UP A LIST
     """
 
-    total: float = 0
-
-    for item in input_list:
-        total += item
-
-    return total
+    return float(sum(input_list))
