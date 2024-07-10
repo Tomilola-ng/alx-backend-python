f708ec8068d94e2f02b6850be1c74f040f01d6c8
Tomilola-ng <tee.o2809@gmail.com>
2024-07-09 21:36:28 -0700

    PEP8

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
         A coroutine that yields 10 random numbers between 0 and 10.
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,11 +8,12 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """
     Collect 10 random numbers using an async
         comprehension over async_generator.
-    
+
     Returns:
         List[float]: A list of 10 random float
             numbers.
