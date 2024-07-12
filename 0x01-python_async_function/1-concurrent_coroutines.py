61e1e10b38928a861958d304ca2e6f74bd7b2af2
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-07-12 22:32:57 +0100

    Rando

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-""" The basics of async """
+
+"""
+Asynchronous Tasks Management
+
+This module demonstrates spawning multiple asynchronous tasks and managing their
+execution and results.
+"""
 
 import asyncio
 from typing import List
@@ -7,9 +13,18 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(task_count: int, max_delay: int) -> List[float]:
     """
-    spawn wait_random n times with the specified max_delay.
+    Spawn 'wait_random' task_count times with the specified max_delay.
+
+    Args:
+    task_count (int): The number of tasks to spawn.
+    max_delay (int): The maximum delay for each task.
+
+    Returns:
+    List[float]: A list of delays for each completed task.
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(task_count)]
     return [await task for task in asyncio.as_completed(tasks)]
+    
