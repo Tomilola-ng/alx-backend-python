83651a074ac4aa6f57c48b10fb1604d8aa9e9c85
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-07-12 22:34:18 +0100

    Tictoc

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" The basics of async """
+"""The basics of async"""
 
 from asyncio import run
 from time import time
@@ -7,12 +7,25 @@ from time import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """ Measure the runtime """
-    start = time()
+def measure_time(task_count: int, max_delay: int) -> float:
+    """
+    Measure the average runtime of executing 'wait_n' tasks.
 
-    run(wait_n(n, max_delay))
+    Args:
+    task_count (int): The number of tasks to spawn.
+    max_delay (int): The maximum delay for each task.
 
-    end = time()
+    Returns:
+    float: The average runtime per task.
+    """
+    start_time = time()
 
-    return (end - start) / n
+    run(wait_n(task_count, max_delay))
+
+    end_time = time()
+
+    total_time = end_time - start_time
+    average_time = total_time / task_count
+
+    return average_time
+    
