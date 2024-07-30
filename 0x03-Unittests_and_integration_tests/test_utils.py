a7583b27fef75e8a748cce1364515eb44e484182
Tomilola-ng <tee.o2809@gmail.com>
2024-07-30 05:34:23 +0100

    DO_IT_LIKE_IT_SHOULD_BE

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,16 +13,17 @@ from utils import *
 class TestAccessNestedMap(unittest.TestCase):
     """Test access_nested_map function"""
     @parameterized.expand([
-        ({"person": {"bio": {"name": "John", "age": 30}}}, ("person", "bio", "name"), "John"),
-        ({"person": {"bio": {"name": "John", "age": 30}}}, ("person", "bio", "age"), 30),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({"person": {"bio": {"name": "John", "age": 30}}}, ("person", "bio", "address"), "address"),
-        ({"person": {"bio": {"name": "John", "age": 30}}}, ("person", "address"), "address"),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
     def test_access_nested_map_with_key_error(self, nested_map, path, expected):
         """Test access_nested_map with KeyError"""
